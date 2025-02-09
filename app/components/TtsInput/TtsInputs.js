a3224dc96488e9337b1f99b4e85e6a66e44e5216
Roshan Shibu <roshanshibu97@gmail.com>
2025-02-09 14:27:08 +0100

    add option to switch language/voices

--- app/components/TtsInput/TtsInputs.js
+++ app/components/TtsInput/TtsInputs.js
@@ -1,5 +1,5 @@
 "use client";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./TtsInput.module.css";
 import { tts_base64 } from "@/app/tts";
 
@@ -9,6 +9,7 @@ export default function TtsInput({ id, onDelete, speechRate, pitch }) {
   const [base64Audio, setBase64Audio] = useState("");
   const [isTTSLoading, setIsTTSLoading] = useState(false);
   const [isConfirmRemoveVisible, setIsConfirmRemoveVisible] = useState(false);
+  const [voice, setVoice] = useState("ml-IN-SobhanaNeural");
 
   const removeConfirmContainerRef = useRef(null);
 
@@ -17,6 +18,35 @@ export default function TtsInput({ id, onDelete, speechRate, pitch }) {
       setIsConfirmRemoveVisible(false);
     }
   };
+
+  const doTTS = (force = false) => {
+    if (isTextChanged || (force && text.length > 0)) {
+      setIsTTSLoading(true);
+      tts_base64(text, speechRate, pitch, voice).then((b64aud) => {
+        setIsTTSLoading(false);
+        setBase64Audio(b64aud);
+        setIsTextChanged(false);
+      });
+    }
+  };
+
+  const handleVoiceChange = (option) => {
+    let optionVoiceMap = {
+      mal: "ml-IN-SobhanaNeural",
+      eng1: "en-US-JennyNeural",
+      eng2: "en-GB-RyanNeural",
+      eng3: "en-AU-NatashaNeural",
+      eng4: "en-US-SteffanNeural",
+      ger1: "de-DE-ConradNeural",
+      ger2: "de-DE-AmalaNeural",
+    };
+    setVoice(optionVoiceMap[option]);
+  };
+
+  useEffect(() => {
+    console.log("voice changed!");
+    doTTS(true);
+  }, [voice]);
 
   return (
     <div className={styles.ttsInputContainer}>
@@ -29,16 +59,7 @@ export default function TtsInput({ id, onDelete, speechRate, pitch }) {
           setIsTextChanged(true);
           setText(e.target.value);
         }}
-        onBlur={() => {
-          if (isTextChanged) {
-            setIsTTSLoading(true);
-            tts_base64(text, speechRate, pitch).then((b64aud) => {
-              setIsTTSLoading(false);
-              setBase64Audio(b64aud);
-              setIsTextChanged(false);
-            });
-          }
-        }}
+        onBlur={doTTS}
       ></textarea>
       {isTTSLoading ? (
         <p>Loading...</p>
@@ -48,15 +69,28 @@ export default function TtsInput({ id, onDelete, speechRate, pitch }) {
           src={`data:audio/mp3;base64,${base64Audio}`}
         />
       )}
-      <div
-        className={styles.removeConfirmContainer}
-        ref={removeConfirmContainerRef}
-        onBlur={handleRemoveConfirmBlur}
-      >
-        <button onClick={() => setIsConfirmRemoveVisible(true)}>Remove</button>
-        {isConfirmRemoveVisible && (
-          <button onClick={() => onDelete(id)}>Confirm</button>
-        )}
+      <div className={styles.footer}>
+        <select onChange={(e) => handleVoiceChange(e.target.value)}>
+          <option value="mal">Malayalam</option>
+          <option value="eng1">English 1</option>
+          <option value="eng2">English 2</option>
+          <option value="eng3">English 3</option>
+          <option value="eng4">English 4</option>
+          <option value="ger1">German 1</option>
+          <option value="ger2">German 2</option>
+        </select>
+        <div
+          className={styles.removeConfirmContainer}
+          ref={removeConfirmContainerRef}
+          onBlur={handleRemoveConfirmBlur}
+        >
+          <button onClick={() => setIsConfirmRemoveVisible(true)}>
+            Remove
+          </button>
+          {isConfirmRemoveVisible && (
+            <button onClick={() => onDelete(id)}>Confirm</button>
+          )}
+        </div>
       </div>
     </div>
   );
--- app/tts.js
+++ app/tts.js
@@ -2,10 +2,10 @@
 
 import { EdgeTTS } from "@andresaya/edge-tts";
 
-export async function tts_base64(input_text, speechRate, pitch) {
+export async function tts_base64(input_text, speechRate, pitch, voice) {
   const tts = new EdgeTTS();
 
-  await tts.synthesize(input_text, "ml-IN-SobhanaNeural", {
+  await tts.synthesize(input_text, voice, {
     rate: `${speechRate}%`, // Speech rate (range: -100% to 100%)
     volume: "100%", // Speech volume (range: -100% to 100%)
     pitch: `${pitch}Hz`, // Voice pitch (range: -100Hz to 100Hz)
